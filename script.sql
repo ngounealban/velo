6468199f03d5c080159b6a344ace5caa96b80a51
ngoune <alban.ngoune@gmail>
2018-12-31 07:19:27 +0100

    ajout de l'obtion HPM et SP dans la bd

--- script.sql
+++ script.sql
@@ -1,60 +1,213 @@
-create table championat(
-    id_championat integer primary key auto_increment,
-    nom_championat varchar(255), 
-    visibilite integer,/*1->visible 0->ivisible*/
-    date_creation date,   
-    date_dabut date,
-    description text,
-    date_fin date
-);
-/* le mot de passe de connexion sera stocke dans la premiere occurence championat*/
-insert into championat (nom_championat) values ('fenapvelo2019');
-create table course(
-	id_course integer primary key auto_increment,
-	id_championat integer references championat (id_championat),
-	type_course int, /*0 -> pour course avec temps, 1 -> course sans temps*/
-	nom_course varchar(255),
-	trajet_course varchar(255),
-	date_creation datetime,
-	detail text,
-	description_course varchar(255)
-);
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  lun. 31 déc. 2018 à 07:12
+-- Version du serveur :  10.1.26-MariaDB
+-- Version de PHP :  7.1.8
 
-create table coureur(
-	id_coureur integer primary key auto_increment,
-	nom_coureur varchar(255),
-	date_inscription date,
-	id_equipe integer references equipe (id_equipe),
-	prenom_coureur varchar(255)	
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-create table equipe (
-	id_equipe integer primary key auto_increment,
-	nom_equipe varchar(255)
-);
 
- create table equipe_coureur(
-	id_coureur integer references coureur (id_coureur),
-	id_equipe integer references equipe (id_equipe),
-	status_chef int default 0, /*0-> membre 1-> chef*/
-	primary key(id_coureur,id_equipe)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-create table info_course
-(
-	id_course integer references course (id_course),
-	id_coureur integer references coureur(id_coureur),
-	temps time,
-	points integer,
-	primary key(id_coureur,id_course)
-);
+--
+-- Base de données :  `velo`
+--
 
-create table coureur_championat
-(
-	id_championat integer references championat (id_championat),
-	id_coureur integer references coureur(id_coureur),
-	temps integer,
-	points time,
-	detail text,
-	primary key(id_coureur,id_championat)
-);
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `championat`
+--
+
+CREATE TABLE `championat` (
+  `id_championat` int(11) NOT NULL,
+  `nom_championat` varchar(255) DEFAULT NULL,
+  `visibilite` int(11) DEFAULT NULL,
+  `date_creation` date DEFAULT NULL,
+  `date_dabut` date DEFAULT NULL,
+  `description` text,
+  `date_fin` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `championat`
+--
+
+INSERT INTO `championat` (`id_championat`, `nom_championat`, `visibilite`, `date_creation`, `date_dabut`, `description`, `date_fin`) VALUES
+(1, '123', NULL, NULL, NULL, NULL, NULL),
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `coureur`
+--
+
+CREATE TABLE `coureur` (
+  `id_coureur` int(11) NOT NULL,
+  `nom_coureur` varchar(255) DEFAULT NULL,
+  `date_inscription` date DEFAULT NULL,
+  `id_equipe` int(11) DEFAULT NULL,
+  `prenom_coureur` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `coureur_championat`
+--
+
+CREATE TABLE `coureur_championat` (
+  `id_championat` int(11) NOT NULL,
+  `id_coureur` int(11) NOT NULL,
+  `id_equipe` int(11) DEFAULT NULL,
+  `temps` varchar(255) DEFAULT NULL,
+  `points` int(11) DEFAULT NULL,
+  `detail` text,
+  `status` int(11) DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `course`
+--
+
+CREATE TABLE `course` (
+  `id_course` int(11) NOT NULL,
+  `id_championat` int(11) DEFAULT NULL,
+  `type_course` int(11) DEFAULT NULL,
+  `nom_course` varchar(255) DEFAULT NULL,
+  `trajet_course` varchar(255) DEFAULT NULL,
+  `date_creation` datetime DEFAULT NULL,
+  `detail` text,
+  `description_course` varchar(255) DEFAULT NULL,
+  `depart` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `equipe`
+--
+
+CREATE TABLE `equipe` (
+  `id_equipe` int(11) NOT NULL,
+  `nom_equipe` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `equipe`
+--
+
+INSERT INTO `equipe` (`id_equipe`, `nom_equipe`) VALUES
+(1, 'einstein'),
+(2, 'albert');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `equipe_coureur`
+--
+
+CREATE TABLE `equipe_coureur` (
+  `id_coureur` int(11) NOT NULL,
+  `id_equipe` int(11) NOT NULL,
+  `status_chef` int(11) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `gpm_sp`
+--
+
+CREATE TABLE `gpm_sp` (
+  `id` int(11) NOT NULL,
+  `id_course` int(11) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `top_3` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `info_course`
+--
+
+CREATE TABLE `info_course` (
+  `id_course` int(11) NOT NULL,
+  `id_coureur` int(11) NOT NULL,
+  `temps` varchar(255) DEFAULT NULL,
+  `points` int(11) DEFAULT NULL,
+  `arrive` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `info_course`
+--
+
+
+ALTER TABLE `championat`
+  ADD PRIMARY KEY (`id_championat`);
+
+--
+-- Index pour la table `coureur`
+--
+ALTER TABLE `coureur`
+  ADD PRIMARY KEY (`id_coureur`);
+
+--
+-- Index pour la table `coureur_championat`
+--
+ALTER TABLE `coureur_championat`
+  ADD PRIMARY KEY (`id_coureur`,`id_championat`);
+
+--
+-- Index pour la table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`id_course`);
+
+--
+-- Index pour la table `equipe`
+--
+ALTER TABLE `equipe`
+  ADD PRIMARY KEY (`id_equipe`);
+
+--
+-- Index pour la table `equipe_coureur`
+--
+ALTER TABLE `equipe_coureur`
+  ADD PRIMARY KEY (`id_coureur`,`id_equipe`);
+
+--
+-- Index pour la table `gpm_sp`
+--
+ALTER TABLE `gpm_sp`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `info_course`
+--
+ALTER TABLE `info_course`
+  ADD PRIMARY KEY (`id_coureur`,`id_course`);
+
